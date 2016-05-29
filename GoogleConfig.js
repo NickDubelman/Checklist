a5944e013f24262dbfeaadedc239f479543fa542
Nick Dubelman <ndubelman@gmail.com>
2016-05-29 00:02:58 -0700

    fixed npm deps and added GoogleConfig

---
+++ GoogleConfig.js
@@ -0,0 +1,12 @@
+// Sets up Google Accounts with API key
+
+/*ServiceConfiguration.configurations.upsert(
+  { service: "google" },
+  {
+    $set: {
+      clientId: "115846289761-mveqp3kqp2mfn6tnf5k2m3o3iq431omg.apps.googleusercontent.com",
+      loginStyle: "redirect",
+      secret: "II7vJKDV5qaYUx9hcfawDrgI"
+    }
+  }
+);*/
--- client/app.js
+++ client/app.js
@@ -7,13 +7,3 @@ Meteor.startup(()=>{
 	render(<Main loggedIn={!!Meteor.userId()}/>, document.getElementById('app'))
 })
 
-/*ServiceConfiguration.configurations.upsert(
-  { service: "google" },
-  {
-    $set: {
-      clientId: "115846289761-mveqp3kqp2mfn6tnf5k2m3o3iq431omg.apps.googleusercontent.com",
-      loginStyle: "redirect",
-      secret: "II7vJKDV5qaYUx9hcfawDrgI"
-    }
-  }
-);*/
